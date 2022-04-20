f7279ff93b2f156b75a674af788885e9ec640aa1
VincenzoBorriello89 <vincborr89@gmail.com>
2022-04-20 15:48:50 +0200

    creaz barra logo e main vuoto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- fascia con logo in alto -->
+    <div class="logo-bar">
+      <img src="./assets/logo-spotify.png" alt="">
+    </div>
+
+    <!-- main centrale con i dischi -->
+    <FolderDisk/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FolderDisk from './components/FolderDisk.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FolderDisk,
   }
 }
 </script>
 
 <style lang="scss">
+// reset
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  background-color: #1e2d3b;
+  height: 100vh;
+  width: 100vw;
+
+  .logo-bar {
+    height: 70px;
+    padding: 10px 20px;
+    background-color: #2e3a46;
+
+    img {
+      height: 100%;
+    }
+  }
+  
 }
 </style>
