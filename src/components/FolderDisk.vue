1d893768f207aac57614a120d99e219107e7f91d
VincenzoBorriello89 <vincborr89@gmail.com>
2022-04-20 16:44:51 +0200

    installato axios e import data

---
+++ src/components/FolderDisk.vue
@@ -0,0 +1,53 @@
+<template>
+
+   <main>
+
+       <section>
+
+           <DiskCard/>
+
+       </section>
+
+   </main>
+    
+</template>
+
+
+
+<script>
+import DiskCard from '@/components/DiskCard.vue';
+
+import axios from "axios";
+
+export default {
+    name: 'FolderDisk',
+    components: {
+        DiskCard,
+    },
+    data(){
+        return{
+            // impostiamo array vuoto da riempire con i dati
+            // del api esterno inerenti ai dischi
+            diskArray:[]
+        }
+    },
+    created(){
+        //qui utilizziamo axios
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+             .then( (res) => {
+                 console.log(res.data.response);
+                 this.diskArray = res.data.response
+             })
+             .catch( (error) => {
+                 console.log( error )
+             })
+    }
+}
+
+</script>
+
+
+
+<style scoped lang="scss">
+
+</style>
